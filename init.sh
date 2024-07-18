572ac5f4aa5cf06cb55ca040a447f23755e5d9c1
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-18 08:29:51 +0300

    feat: use replacer

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@
 read -p "Enter a description:" description
 read -p "Enter your username:" author
 
-# LC_CTYPE=C && LANG=C && find ./ \( -iname \*.js -o -iname \*.md -o -iname \*.json \) -print0 | xargs -0 sed -i '' "s/<project>/kostas/g"
-
 sed -i '' "s/<description>/$description/g" package.json
 sed -i '' "s/<author>/${author//@/}/g" package.json
 sed -i '' "s/<author>/${author//@/}/g" LICENSE
 sed -i '' "s/<year>/$(date +%Y)/g" LICENSE
+
+LC_CTYPE=C && LANG=C && find ./ \( -iname \*.js -o -iname \*.md -o -iname \*.json \) -print0 | xargs -0 sed -i '' "s/<project>/kostas/g"
 
 npm i -D @eslint/js globals
 
